6a956a419ab95a01e8f6faec27380b88873794ce
hotshot07 <aroram@tcd.ie>
2022-03-10 01:51:46 +0000

    minor changes

--- pcap-service/process_pcap.py
+++ pcap-service/process_pcap.py
@@ -30,45 +30,44 @@ def send_file(path, service, port):
     with open(path, "rb") as file:
         file_dict = {'uploaded_file': file}
         try :
-            response = requests.post(f"{service}:{port}/file", files=file_dict)
+            response = requests.post(f"http://{service}:{port}/file", files=file_dict)
             return response.status_code
         except Exception as e:
             print(e) 
 
-if __name__ == '__main__':
 
-    try:
-        while True:
-            new_files = check_new_files('./received-files/')
+try:
+    while True:
+        new_files = check_new_files('./received-files/')
 
-            if len(new_files)!=0:
-                for file in sorted(new_files):
-                    if str(file).endswith('.pcap'):
-                        jobs.put(file)
-                        print(f"{file} is in queue")
+        if len(new_files)!=0:
+            for file in sorted(new_files):
+                if str(file).endswith('.pcap'):
+                    jobs.put(file)
+                    print(f"{file} is in queue")
                     
-            time.sleep(2)
-            if not jobs.empty():
-                pcap_path = jobs.get()
+        time.sleep(2)
+        if not jobs.empty():
+            pcap_path = jobs.get()
                 
-                #send file to data-service to collect all csv's
-                data_service_status_code = send_file(pcap_path, DATA_SERVICE, DATA_PORT)
+            #send file to data-service to collect all csv's
+            data_service_status_code = send_file(pcap_path, DATA_SERVICE, DATA_PORT)
                 
-                #send converted 
-                converted_csv_path = convert_pcap_to_csv(pcap_path)
+            #send converted 
+            converted_csv_path = convert_pcap_to_csv(pcap_path)
                 
-                #send file to model-service for processing
-                model_service_status_code = send_file(pcap_path, MODEL_SERVICE,MODEL_PORT)
+            #send file to model-service for processing
+            model_service_status_code = send_file(pcap_path, MODEL_SERVICE,MODEL_PORT)
                 
-                if model_service_status_code == '200' and data_service_status_code == '200':
-                    os.remove(pcap_path)
-                else:
-                    jobs.put(pcap_path)
+            if model_service_status_code == '200':
+                os.remove(pcap_path)
+            else:
+                jobs.put(pcap_path)
                     
-    except KeyboardInterrupt:
-        print("\nQuitting the program.")
-    except Exception as e:
-        print(e)
+except KeyboardInterrupt:
+    print("\nQuitting the program.")
+except Exception as e:
+    print(e)
 
 
     
